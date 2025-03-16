5ba9d9604a5c654ce9fa4f76e555c51243021d59
EduAndr3s <eduardoandres.segoviaroman@cieep.com>
2025-03-16 10:11:31 +0100

    first class, configuration

---
+++ my-app-react-with-ts/src/app/page.tsx
@@ -0,0 +1,13 @@
+
+export default function Home() {
+  return (
+    <div >
+      <main>
+        <h1 className="text-3xl font-bold underline">Hello World</h1>
+      </main>
+      <footer>
+        <p className="text-3xl font-bold underline">Footer</p>
+      </footer>
+    </div>
+  );
+}

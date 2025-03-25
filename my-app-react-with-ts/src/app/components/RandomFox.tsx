fe79d9fb651e41515761e2305673649a44c11c59
EduAndr3s <eduardoandres.segoviaroman@cieep.com>
2025-03-25 20:06:00 +0100

    curso en 50% de progreso

--- my-app-react-with-ts/src/app/components/RandomFox.tsx
+++ my-app-react-with-ts/src/app/components/RandomFox.tsx
@@ -1,11 +1,13 @@
 //generate a random function to get a random number between 1 and 123
-const random = () => Math.floor(Math.random() * 123) + 1;
 
-export const RandomFox = ():JSX.Element => {
+type Props ={image: string}
 
-    const image = `https://randomfox.ca/images/${random()}.jpg`;
 
-    return <img width={320} height={180} src={image} alt="Random Fox" className="rounded-md"/>
+export const RandomFox = ({image} : Props): JSX.Element => {
+
+  
+
+    return <img width={320} height='auto' src={image} alt="Random Fox" className="rounded-md"/>
 };
 
 
--- my-app-react-with-ts/src/app/page.tsx
+++ my-app-react-with-ts/src/app/page.tsx
@@ -1,10 +1,44 @@
+"use client";
+import { MouseEventHandler, useState } from "react";
 import { RandomFox } from "./components/RandomFox";
+
+const random = (): number => Math.floor(Math.random() * 123) + 1;
+
+
+const generateId = () => Math.random().toString(36).substring(2,9);
+
+type ImageItems = {id: string, url: string};
+
 export default function Home() {
+  const [images, setImages] = useState<Array<ImageItems>>([])
+  
+  const addNewFox: MouseEventHandler<HTMLButtonElement> = (event) => {
+
+    event.preventDefault()
+
+      const newImageItem: ImageItems = {
+        id: generateId(),
+        url: `https://randomfox.ca/images/${random()}.jpg`
+      }
+      setImages([
+        ...images,
+        newImageItem
+      ])
+    }
   return (
     <div >
       <main>
         <h1 className="text-3xl font-bold underline">Hello World</h1>
-        <RandomFox />
+        <button onClick={addNewFox}>Add New Fox</button>
+      {images.map(({id, url})=>(
+              <div key={id} className="p-4">
+
+                  <RandomFox image = {url} />
+
+              </div>
+
+      ))}
+
       </main>
       <footer>
         <p className="text-3xl font-bold underline">Footer</p>
